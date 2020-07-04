f7398cc4e7bf0725fb97aa91ffe070d394cfea31
Alex <alexzlkl@yandex.ru>
2020-07-04 17:07:52 +0300

    add full munu + fix bags

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('#hamburger');
+const body = document.body;
+const links = document.querySelectorAll('.menu__link');
+const overlayElement = document.querySelector('.overlay');
+
+
+
+
+links.forEach(function(element){
+    element.addEventListener('click' , toggleMenu);
+  })
+
+
+function toggleMenu(){
+    hamburger.classList.toggle('burger--active');
+    overlayElement.classList.toggle('overlay--active');
+    body.classList.toggle('body--active-menu');
+  }
+
+hamburger.addEventListener('click' , toggleMenu);
