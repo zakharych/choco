7b06402a8cc0ae96188b51a62f4153537ac63743
zakharych <alexzlk@yandex.ru>
2020-07-13 18:34:02 +0300

    review js

---
+++ js/review.js
@@ -0,0 +1,17 @@
+const findBlockByAlias = (alias) =>{
+    return $(".review").filter((ndx, item) => {
+        return $(item).attr(".data-linked") === alias;
+    })
+}
+$(".reviews-autors__item").click((e) =>{
+    
+    const $this = $(e.curentTarget);
+    const target = $this.attr("data-open");
+    const itemToShow = findBlockByAlias(target);
+    const curItem = $this.closest(".reviews-autors__item");
+
+    itemToShow.addClass("active").siblings().removeClass("active");
+    curItem.addClass("active").siblings().removeClass("active");
+}
+
+)
